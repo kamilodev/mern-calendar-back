ba6e88bb4fd1b19df4f663c3edae314b5af24ece
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-22 22:38:42 +0200

    Handling errors with express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
 const { response } = require('express')
+const { validationResult } = require('express-validator')
 
 const createUser = (req, res = response) => {
 	const { name, email, password } = req.body
 
-	res.json({
+	// Error handling
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			ok: false,
+			errors: errors.mapped(),
+		})
+	}
+
+	res.status(200).json({
 		ok: true,
 		msg: 'register',
 		name,
@@ -14,7 +24,17 @@ const createUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
 	const { email, password } = req.body
-	res.json({
+
+	// Error handling
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(400).json({
+			ok: false,
+			errors: errors.mapped(),
+		})
+	}
+
+	res.status(200).json({
 		ok: true,
 		msg: 'login',
 		email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,37 @@
 */
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth')
+const { check } = require('express-validator')
 
 const { Router } = require('express')
 const router = Router()
 
-router.post('/new', createUser)
+router.post(
+	'/new',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Email is required').isEmail(),
+		check('password', 'Password is required').not().isEmpty(),
+		check(
+			'password',
+			'Password minimum length must be at least 8 characters',
+		).isLength({ min: 8 }),
+	],
+	createUser,
+)
 
-router.post('/', loginUser)
+router.post(
+	'/',
+	[
+		check('email', 'Email is required').isEmail(),
+		check('password', 'Password is required').not().isEmpty(),
+		check(
+			'password',
+			'Password minimum length must be at least 8 characters',
+		).isLength({ min: 8 }),
+	],
+	loginUser,
+)
 
 router.get('/renew', renewToken)
 
