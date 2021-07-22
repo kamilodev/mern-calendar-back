48a04777807f8061b9be9d960b9ff00bc7ea61fb
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-22 12:38:03 +0200

    Index HTML created and environment variables

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
+require('dotenv').config()
+
 
 // Create express server
 const app = express()
 
+//Public Dir
+app.use(express.static('public'));
+
 
 //Routes
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     console.log('First Route Installed')
     res.json({
         ok: true
     })
-})
+})*/
 
 // Listeners
-app.listen(4000, () => {
-    console.log(`Server on port ${4000}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server on port ${process.env.PORT}`)
 })
