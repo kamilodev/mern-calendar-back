c28387446ed9de8c2ea4d4b16c1373e8ef46fc9c
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-22 22:08:38 +0200

    Retrieve information from a post request

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,28 @@
 const { response } = require('express')
 
 const createUser = (req, res = response) => {
+	const { name, email, password } = req.body
+
 	res.json({
 		ok: true,
 		msg: 'register',
+		name,
+		email,
+		password,
 	})
 }
 
-const loginUser = (req, res) => {
+const loginUser = (req, res = response) => {
+	const { email, password } = req.body
 	res.json({
 		ok: true,
 		msg: 'login',
+		email,
+		password,
 	})
 }
 
-const renewToken = (req, res) => {
+const renewToken = (req, res = response) => {
 	res.json({
 		ok: true,
 		msg: 'renew',
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express()
 //Public Dir
 app.use(express.static('public'))
 
+// Read and parse of body
+app.use(express.json())
+
 // Routes
 app.use('/api/auth', require('./routes/auth'))
 
