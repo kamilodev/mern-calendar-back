bc935c7e87868eaef241331256ee2a8320b455cb
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-22 21:00:08 +0200

    Creating users route

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 require('dotenv').config()
 
-
 // Create express server
 const app = express()
 
 //Public Dir
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // Routes
-
+app.use('/api/auth', require('./routes/auth'))
 
 // Listeners
 app.listen(process.env.PORT, () => {
-    console.log(`Server on port ${process.env.PORT}`)
+	console.log(`Server on port ${process.env.PORT}`)
 })
--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,15 @@
-# Auth file
+/*
+   User Routes /Auth
+   host + /api/auth
+*/
+
+const { Router } = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+	res.json({
+		ok: true,
+	})
+})
+
+module.exports = router
