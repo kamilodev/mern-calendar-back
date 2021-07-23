7bf4d1a952e6e6bd81ecf4a98ba0da477a5d8a13
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-23 12:42:16 +0200

    Validating user from email

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,25 @@ const { response } = require('express')
 const User = require('../models/User')
 
 const createUser = async(req, res = response) => {
-	//const { name, email, password } = req.body
+	const { email, password } = req.body
 	try {
-		const user = new User(req.body)
+		let user = await User.findOne({email})
+		if (user) {
+			res.status(400).json({
+				ok: false,
+				msg: 'Email user registered in dataBase',
+			})
+		}
+
+		user = new User(req.body)
 		await user.save()
 
 		res.status(201).json({
 			ok: true,
-			msg: 'User registered in dataBase',
+			uid: user.id,
+			name: user.name,
 		})
+
 	} catch (error) {
 		res.status(500).json({
 			ok: false,
