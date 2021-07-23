f325699cb8196ef394532e91ddf91325befce718
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2021-07-23 13:31:23 +0200

    User login created and validated

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,15 +35,40 @@ const createUser = async(req, res = response) => {
 	}
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
 	const { email, password } = req.body
 
-	res.status(200).json({
-		ok: true,
-		msg: 'login',
-		email,
-		password,
-	})
+	try {
+		let user = await User.findOne({email})
+		if (!user) {
+			res.status(400).json({
+				ok: false,
+				msg: 'Wrong user or email',
+			})
+		}
+
+		// Confirm Password
+		const validPassword = bcrypt.compareSync(password, user.password)
+
+		if( !validPassword ) {
+			return res.status(400).json({
+				ok: false,
+				msg: 'Wrong user or email',
+			})
+		}
+
+		res.json({
+			ok: true,
+			uid: user.id,
+			name: user.name,
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({
+			ok: false,
+			msg: 'Contact with admin database'
+		})
+	}
 }
 
 const renewToken = (req, res = response) => {
