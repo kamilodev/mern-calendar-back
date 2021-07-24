6562ba8cc478c79430cef63964440aeb72153053
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-24 20:17:59 +0200

    Obtain all events

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,12 @@
 const { response } = require('express')
 const Event = require('../models/Event')
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+	const events = await Event.find().populate('user', 'name email')
+
 	res.json({
 		ok: true,
-		msg: 'getEvents',
+		events,
 	})
 }
 
