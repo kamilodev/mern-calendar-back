75f8b5930ee7f59eb18cdc005f6e61f4632a88d2
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-24 19:38:55 +0200

    Validate fields and data before createEvent

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,8 @@
 const { Router } = require('express')
 const { validateJWT } = require('../middlewares/validate-jwt')
+const { fieldValidator } = require('../middlewares/field-validator')
+const { check } = require('express-validator')
+const { isDate } = require('../helpers/isDate')
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events')
 
 const router = Router()
@@ -11,7 +14,16 @@ router.use(validateJWT)
 router.get('/', getEvents)
 
 // READ
-router.post('/', createEvent)
+router.post(
+	'/',
+	[
+		check('title', 'You should be enter a title').not().isEmpty(),
+		check('start', 'Start date is mandatory').custom(isDate),
+		check('end', 'End date is mandatory').custom(isDate),
+	],
+	fieldValidator,
+	createEvent,
+)
 
 // UPDATE
 router.put('/:id', updateEvent)
